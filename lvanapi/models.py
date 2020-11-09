446a50ea4607cb9ea039725377180845fd22825d
aikeke <1025252757@qq.com>
2020-11-09 17:56:16 +0800

    修改使用mysql功能

--- lvanapi/models.py
+++ lvanapi/models.py
@@ -5,3 +5,11 @@ class UserInfo(models.Model):
     password=models.CharField(max_length=32)
 
 # Create your models here.
+class HostInfo(models.Model):
+    hostname=models.CharField(max_length=32)
+    inner_ip=models.CharField(max_length=16)
+    outer_ip=models.CharField(max_length=32)
+    cpu=models.CharField(max_length=2)
+    memory=models.CharField(max_length=3)
+    disk_size=models.CharField(max_length=3)
+    
--- lvanops/settings.py
+++ lvanops/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'lvanops.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lvanops',
+        'USER':'root',
+        'PASSWORD':'iwgame.hl100',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
     }
 }
 
