197b0d09b9a2610ed8e4a81d81ad8d79648cdae1
aikeke <1025252757@qq.com>
2020-12-17 17:34:05 +0800

    使用多进程修改防火墙策略，尝试添加一些js

--- src/modify_iptables.py
+++ src/modify_iptables.py
@@ -5,6 +5,7 @@
 from ssh_connect import Ssh
 from common import get_redis
 from get_logger import get_logger
+from mul_pool import Mul_pool
 
 game_inside_cmd=''' sed -i -e '/28880/s/^/#/' -e '/28881/s/^/#/' -e '/28882/s/^/#/' -e '/28883/s/^/#/' -e '/38887/s/^/#/' -e '/48887/s/^/#/' -e '/58887/s/^/#/' -e '/28885/s/^/#/' -e '/28958/s/^/#/' -e '/9002/s/^/#/' -e '/9003/s/^/#/' -e '/9005/s/^/#/' -e '/9006/s/^/#/' -e '/28886/s/^/#/' -e '/28887/s/^/#/' /etc/sysconfig/iptables '''
 
@@ -14,10 +15,8 @@ login_inside_cmd=''' sed -i -e '/28885/s/^/#/' -e '/28958/s/^/#/' /etc/sysconfig
 
 login_outer_cmd=''' sed -i -e '/28885/s/^#\{1,50\}//' -e '/28958/s/^#\{1,50\}//' /etc/sysconfig/iptables '''
 
-ans={}
 
-def modify_iptables(cmd_str,key,value,port=2208,user="lvantech"):
-    global ans
+def modify_iptables(res,cmd_str,key,value,port=2208,user="lvantech"):
     redis_obj=get_redis()
     ssh_obj=Ssh(value,2208,user,redis_obj.hget(user,value))
     try:
@@ -27,36 +26,36 @@ def modify_iptables(cmd_str,key,value,port=2208,user="lvantech"):
             cmd_str='''/etc/init.d/iptables restart '''
             result,err=ssh_obj.cmd(cmd_str,sudo=True)
             if err.strip():
-                ans[key]=err
+                res[key]=err
             else:
-                ans[key]='success'
+                res[key]=result
     except Exception as e:
-        ans[key]='pass is wrong'
+        res[key]='pass is wrong'
         pass
     #get_logger('modify_iptables').info(ans)
 
 def excute_iptables(argv):
     redis_obj=get_redis()
+    p=Mul_pool()
+    res=p.get_dict()
     if argv=='gmoff':
         data=redis_obj.hgetall('smgame_server')
         for key,value in data.items():
-            modify_iptables(game_inside_cmd,key,value)
-        get_logger('modify_iptables').info(ans)
+            p.get_pool(modify_iptables,res,game_inside_cmd,key,value)
     elif argv=='gmon':
         data=redis_obj.hgetall('smgame_server')
         for key,value in data.items():
-            modify_iptables(game_outer_cmd,key,value)
-        get_logger('modify_iptables').info(ans)
+            p.get_pool(modify_iptables,res,game_outer_cmd,key,value)
     elif argv=='lgoff':
         data=redis_obj.hgetall('smlogin_server')
         for key,value in data.items():
-            modify_iptables(login_inside_cmd,key,value)
-        get_logger('modify_iptables').info(ans)
+            p.get_pool(modify_iptables,res,login_inside_cmd,key,value)
     elif argv=='lgon':
         data=redis_obj.hgetall('smlogin_server')
         for key,value in data.items():
-            modify_iptables(login_outer_cmd,key,value)
-        get_logger('modify_iptables').info(ans)
+            p.get_pool(modify_iptables,res,login_outer_cmd,key,value)
+    p.Close()
+    get_logger('modify_iptables').info(res)
 
 if __name__=='__main__':
     prompt='''please input your choice:
@@ -66,6 +65,8 @@ if __name__=='__main__':
 4: 所有登录对外
 '''
     argv=raw_input(prompt)
+    p=Mul_pool()
+    res=multiprocessing.Manager().dict()
     redis_obj=get_redis()
     if argv=='1':
         data=redis_obj.hgetall('smgame_server')
@@ -82,7 +83,8 @@ if __name__=='__main__':
     elif argv=='4':
         data=redis_obj.hgetall('smlogin_server')
         for key,value in data.items():
-            result=modify_iptables(login_outer_cmd,key,value)
+            p.get_pool(modify_iptables,res,login_outer_cmd,key,value)
+        p.Close()
+        print(res)
     else:
         exit("输入异常")
-    print(ans)
--- src/mul_pool.py
+++ src/mul_pool.py
@@ -5,10 +5,14 @@ class Mul_pool(object):
     def __init__(self):
         self.cores=multiprocessing.cpu_count()
         self.pool=multiprocessing.Pool(processes=self.cores)
+        self.res=multiprocessing.Manager().dict()
     
     def get_pool(self,f,*args):
         return self.pool.apply_async(f,tuple(args))
+
+    def get_dict(self):
+        return self.res
         
-    def close(self):
+    def Close(self):
         self.pool.close()
         self.pool.join()
